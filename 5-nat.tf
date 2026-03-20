ac8d76376a6ab28f4f28edc5aa5d1a04e86f6632
pjwesley <pjwesley7@gmail.com>
2026-03-20 19:47:06 +0530

    added some new files

--- 5-nat.tf
+++ 5-nat.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "nat" {
   domain = "vpc"
   tags = {
-    Name = "${local-env}-nat"
+    Name = "${local.env}-nat"
   }
 }
 
--- 6-route.tf
+++ 6-route.tf
@@ -1,26 +1,26 @@
 resource "aws_route_table" "private" {
     vpc_id = aws_vpc.main.id
 
-    route = {
+    route {
         cidr_block = "0.0.0.0/0"
         nat_gateway_id = aws_nat_gateway.nat.id
     }
 
     tags = {
-        Name = "${local-env}-private"
+        Name = "${local.env}-private"
     }
 }
 
 resource "aws_route_table" "public" {
     vpc_id = aws_vpc.main.id
 
-    route = {
+    route {
         cidr_block = "0.0.0.0/0"
         nat_gateway_id = aws_internet_gateway.igw.id
     }
 
     tags = {
-        Name = "${local-env}-public"
+        Name = "${local.env}-public"
     }
 }
 
@@ -29,17 +29,17 @@ resource "aws_route_table_association" "private_zone1" {
     route_table_id = aws_route_table.private.id
 }
   
-resource "aws_subnet_table_association" "private_zone2" {
+resource "aws_route_table_association" "private_zone2" {
     subnet_id = aws_subnet.private_zone2.id
     route_table_id = aws_route_table.private.id
 }
 
-resource "aws_subnet_table_association" "public_zone1" {
+resource "aws_route_table_association" "public_zone1" {
     subnet_id = aws_subnet.public_zone1.id
     route_table_id = aws_route_table.public.id
 }
 
-resource "aws_subnet_table_association" "public_zone2" {
+resource "aws_route_table_association" "public_zone2" {
     subnet_id = aws_subnet.public_zone2.id
     route_table_id = aws_route_table.public.id
 }
